0fb786e0482b3abc4c38ce1b6256677128a1f778
fahadpj23 <fahadpj23@gmail.com>
2024-10-30 12:29:43 +0530

    singleItem

--- src/pages/singleItem/index.tsx
+++ src/pages/singleItem/index.tsx
@@ -9,8 +9,8 @@ const SingleItem = () => {
     100;
   return (
     <div className="block md:flex items-center ">
-      <div className="flex justify-center w-1/2">
-        <div className="w-[60vw] h-[80vw] md:w-[26vw] md:h-[35vw]  items-center  ">
+      <div className="flex justify-center w-full md:w-1/2 ">
+        <div className="w-[80vw] h-[80vw] md:w-[26vw] md:h-[35vw] flex justify-center items-center  ">
           <img
             src={product?.image}
             alt="product Image"
@@ -18,16 +18,15 @@ const SingleItem = () => {
           />
         </div>
       </div>
-      <div className="ml-6 w-1/2">
-        <div className="flex space-x-4">
-          <h1 className="font-semibold text-sm md:text-lg text-green-600 tracking-wider">
-            Offer Price: ₹{product.salesPrice}.00
-          </h1>
-          <h1 className=" bg-blue-500 text-white rounded p-1  text-xs md:text-base">
-            {Math.floor(DiscountPercentage)}%off
-          </h1>
-        </div>
-        <h1 className="line-through text-[11px] md:text-base text-red-500">
+      <div className="ml-6 w-1/2 space-y-2">
+        <h1 className=" text-green-500 rounded p-1 font-semibold  text-sm md:text-base">
+          Extra ₹{product?.mrp - product?.salesPrice} off
+        </h1>
+        <h1 className="font-semibold text-sm md:text-lg text-black-600 tracking-wider">
+          Offer Price: ₹{product.salesPrice}.00
+        </h1>
+
+        <h1 className="line-through text-[11px] md:text-base text-gray-500">
           MRP: ₹{product.mrp}
         </h1>
       </div>
