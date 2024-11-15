a39e6eb2384691b165fcfa4474ac12d32cc589fd
fahadpj23 <fahadpj23@gmail.com>
2024-11-15 11:09:28 +0530

    launvh date

--- src/utils/productListSort.ts
+++ src/utils/productListSort.ts
@@ -1,9 +1,14 @@
 export const ProductListSort = (productList: any, sortValue: any) => {
   if (sortValue === "newest") {
-    const products = productList?.sort((a: any, b: any) => {
-      const numA = parseInt(a.id.match(/\d+/)[0], 10);
-      const numB = parseInt(b.id.match(/\d+/)[0], 10);
-      return numA - numB;
+    // const products = productList?.sort((a:any, b:any) => new Date(a.launchDate) - new Date(b.launchDate));
+    const products = productList.sort((a: any, b: any) => {
+      const [dayA, monthA, yearA] = a.launchDate.split("-").map(Number);
+      const [dayB, monthB, yearB] = b.launchDate.split("-").map(Number);
+
+      const dateA = new Date(yearA, monthA - 1, dayA).getTime();
+      const dateB = new Date(yearB, monthB - 1, dayB).getTime();
+
+      return dateB - dateA;
     });
     return products;
   } else if (sortValue === "HighToLow") {
