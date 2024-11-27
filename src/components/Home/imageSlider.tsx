4e56f0462f3a504d3b60d717c252a3239e3f972e
fahadpj23 <fahadpj23@gmail.com>
2024-11-27 17:25:36 +0530

    new slider

--- src/components/Home/imageSlider.tsx
+++ src/components/Home/imageSlider.tsx
@@ -1,24 +1,26 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
-import { MAINBANNER } from "constants/mainBanner";
+import React from "react";
+import Slider from "react-slick";
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 interface props {
   bannerItems: any;
 }
 const ImageSlider: FC<props> = ({ bannerItems }) => {
-  return (
-    <div className="h-[45vw] md:h-[30vw] -mt-3 md:mt-3">
-      <Carousel
-        autoPlay={true}
-        showThumbs={false}
-        showIndicators={true}
-        infiniteLoop={true}
-        showStatus={false}
-        showArrows={false}
-      >
-        {bannerItems?.map((banner: any) => (
+  // var settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  // };
+  // return (
+  //   <div className="h-[45vw] md:h-[30vw] flex">
+  //     <Slider {...settings}>
+  {
+    /* {bannerItems?.map((banner: any) => (
           <Link
             to={banner?.name}
             key={banner?.id}
@@ -37,9 +39,51 @@ const ImageSlider: FC<props> = ({ bannerItems }) => {
               )}
             </div>
           </Link>
-        ))}
-      </Carousel>
-    </div>
+        ))} */
+  }
+  //     <div>1</div>
+  //     <div>1</div>
+  //     <div>1</div>
+  //     <div>1</div>
+  //   </Slider>
+  // </div>
+
+  // );
+  const imageStyle = {
+    width: "180px",
+    height: "80px",
+    borderRadius: "5px",
+  };
+  const settingsSlider = {
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    swipeToSlide: true,
+    infinite: true,
+    dots: true,
+  };
+  return (
+    // <div className=" h-[45vw] md:h-[30vw] mt-2">
+    <Slider {...settingsSlider}>
+      {bannerItems?.map((banner: any) => (
+        <Link to={banner?.name} key={banner?.id}>
+          <div className="relative">
+            <img
+              src={banner?.image}
+              // className="h-[45vw] md:h-[25vw] w-full  rounded-none md:rounded-xl  "
+              alt="banner"
+            />
+            {banner?.link && (
+              <button className="bg-gradient-to-r from-orange-500 via-orange-600 to-orange-700 p-1 rounded-lg text-white text-[11px] absolute left-5 bottom-4">
+                {banner?.link?.title}
+              </button>
+            )}
+          </div>
+        </Link>
+      ))}
+    </Slider>
+    // </div>
   );
 };
 export default ImageSlider;
