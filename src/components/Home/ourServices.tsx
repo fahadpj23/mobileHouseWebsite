2ba7878b98f347b217822dec91c0b5fdf7af8b7a
fahadpj23 <fahadpj23@gmail.com>
2024-11-30 10:24:20 +0530

    footer updates

--- src/components/Home/ourServices.tsx
+++ src/components/Home/ourServices.tsx
@@ -7,9 +7,9 @@ const OurServices = () => {
       <img
         src={image}
         alt="service image"
-        className="rounded-2xl h-44 md:h-56 w-[80vw] md:w-[20vw]"
+        className="rounded-lg md:rounded-2xl h-44 md:h-56 w-[80vw] md:w-[20vw]"
       />
-      <h1 className="font-semibold text-xl">{title}</h1>
+      <h1 className="font-semibold text-base md:text-xl">{title}</h1>
       <h1 className="text-sm w-2/3 md:w-[14vw] text-gray-500 text-center">
         {content}
       </h1>
@@ -17,7 +17,7 @@ const OurServices = () => {
   );
   return (
     <div className="w-full flex flex-col items-center my-3">
-      <h1 className="text-2xl font-semibold uppercase  my-4 text-white  bg-blue-400 p-2 tracking-wider">
+      <h1 className="text-lg md:text-2xl font-semibold uppercase  my-4 text-white  bg-blue-400 p-1 md:p-2 tracking-wider">
         Our Services
       </h1>
       <div className="block md:flex space-x-0 md:space-x-10 space-y-10 md:space-y-0">
