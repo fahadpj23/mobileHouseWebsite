2b84c462c4b7eff3d9e5529be4e19d59a581f13e
fahadpj23 <fahadpj23@gmail.com>
2024-12-06 18:37:41 +0530

    mini banner

--- src/components/Home/banner.tsx
+++ src/components/Home/banner.tsx
@@ -1,18 +1,25 @@
 import { Carousel } from "react-responsive-carousel";
 import { MINIBANNERS } from "constants/miniBannerItem";
 import ImageSlider from "./imageSlider";
+import { Link } from "react-router-dom";
 const Banner = () => {
   return (
     <div>
       <div className="hidden justify-between space-x-5  md:flex">
         {MINIBANNERS?.map((banner: any) => {
           return (
-            <img
-              key={banner?.name}
-              src={banner?.image}
-              alt="banner"
-              className="w-full h-[30vh]"
-            />
+            <Link
+              to={`series/${banner?.series}`}
+              key={banner?.id}
+              style={{ margin: "3px 8px" }}
+            >
+              <img
+                key={banner?.name}
+                src={banner?.image}
+                alt="banner"
+                className="w-full h-[30vh]"
+              />
+            </Link>
           );
         })}
       </div>
--- src/constants/miniBannerItem.ts
+++ src/constants/miniBannerItem.ts
@@ -3,7 +3,7 @@ import Banner2 from "assets/miniBanner/banner2.jpg";
 import Banner3 from "assets/miniBanner/banner3.jpg";
 
 export const MINIBANNERS = [
-  { id: 1, name: "Banner1", image: Banner1 },
-  { id: 2, name: "Banner2", image: Banner2 },
-  { id: 3, name: "Banner3", image: Banner3 },
+  { id: 1, name: "Banner1", image: Banner1, series: ["Y300"] },
+  { id: 2, name: "Banner2", image: Banner2, series: ["A165G"] },
+  { id: 3, name: "Banner3", image: Banner3, series: ["K12X"] },
 ];
