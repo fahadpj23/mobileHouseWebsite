206cdbe5fb4bf24db1635ae3d57cf77adc4dea10
fahadpj23 <fahadpj23@gmail.com>
2024-12-25 12:25:06 +0530

    design

--- src/components/commonComponents/SingleProductCard.tsx
+++ src/components/commonComponents/SingleProductCard.tsx
@@ -14,21 +14,22 @@ const SingleProductCard: FC<props> = ({ product }) => {
 
   return (
     <Link to={`/phone/${product?.id}/${encodeURIComponent(product?.name)}`}>
-      <div className=" p-3 md:p-6 flex flex-col space-y-2  bg-white relative rounded-md">
-        <div className="flex justify-center bg-grayBackground rounded-md shadow-md p-2">
+      <div className=" p-3 md:p-6 flex flex-col space-y-1   bg-white relative rounded-md">
+        <div className="flex justify-center bg-[#f9f6f6] rounded-md shadow-md p-2">
           <div className="w-full h-[150px]  md:h-[170px] object-contain">
             <LazyImage src={product?.image} alt="Product Image" />
           </div>
         </div>
-        <div className="flex flex-col space-y-1">
-          <h1 className="font-semibold  w-full text-[13px] md:text-base truncate">
+
+        <div className="flex flex-col space-y-1 p-1 ">
+          <h1 className="font-semibold  w-full  truncate capitalize text-[12px] ">
             {product?.name}
           </h1>
           <h1 className="absolute top-3 right-3 bg-blue-500 text-white rounded p-1  text-[10px] md:text-base">
             {Math.floor(DiscountPercentage)} %off
           </h1>
           <div className="flex space-x-3 items-center">
-            <h1 className="font-semibold tracking-wider text-[14px] md:text-lg ">
+            <h1 className=" font-medium text-[12px] tracking-wider border-2 border-green-500 p-1 text-green-500 bg-white rounded-md">
               ₹{product.salesPrice}.00
             </h1>
             <h1 className="line-through text-[11px] md:text-[13px] text-gray-500">
