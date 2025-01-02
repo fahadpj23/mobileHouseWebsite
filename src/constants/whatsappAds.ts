e69d6c47421c21abe2dfe00cb160cf2104514045
fahadpj23 <fahadpj23@gmail.com>
2025-01-02 16:27:27 +0530

    price chnage

--- src/constants/whatsappAds.ts
+++ src/constants/whatsappAds.ts
@@ -1,5 +1,4 @@
 import whatsAppImage1 from "assets/whatsappAds/whatsAppImage1.jpeg";
-import whatsAppImage2 from "assets/whatsappAds/whatsAppImage2.jpeg";
 import whatsAppImage3 from "assets/whatsappAds/whatsAppImage3.jpeg";
 import whatsAppImage4 from "assets/whatsappAds/whatsAppImage4.jpeg";
 import whatsAppImage5 from "assets/whatsappAds/whatsAppImage5.jpeg";
@@ -9,11 +8,6 @@ export const WhatappAdsList = [
     id: 1,
     image: whatsAppImage1,
     series: ["REALME14X"],
-  },
-  {
-    id: 2,
-    image: whatsAppImage2,
-    series: ["F155G"],
   },
 
   {
