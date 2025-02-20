25169781ef2cc90f9fe38a0892bdb6d4cbfb3fba
fahadpj23 <fahadpj23@gmail.com>
2025-02-20 10:34:44 +0530

    zoom

--- src/components/commonComponents/productImageSlider.tsx
+++ src/components/commonComponents/productImageSlider.tsx
@@ -1,9 +1,19 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-import { FC } from "react";
+import { FC, useState } from "react";
 import LazyImage from "./imageLazyLoading";
+import { Modal } from "@mui/material";
+import ImageZoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 const ProductImageSlider: FC<any> = ({ productImages }) => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [modalImage, setModalImage] = useState<string>("");
+
+  const handleOpenModal = (image: string) => {
+    setModalOpen(true);
+    setModalImage(image);
+  };
   return (
     <div className="w-[70vw] flex items-center justify-center">
       {productImages?.images?.length ? (
@@ -18,13 +28,31 @@ const ProductImageSlider: FC<any> = ({ productImages }) => {
             selectedItem={0}
           >
             {productImages?.images?.map((image: any) => (
-              <div key={image} className="pb-8 h-[80vw] w-full ">
+              <div
+                onClick={() => setModalOpen(true)}
+                key={image}
+                className="pb-8 h-[80vw] w-full "
+              >
                 <LazyImage src={image} alt="Product Image" />
               </div>
             ))}
           </Carousel>
         </div>
       ) : null}
+      <Modal
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+        aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
+      >
+        <ImageZoom>
+          <img
+            src={modalImage}
+            alt={`Slide 1`}
+            style={{ width: "100%", cursor: "pointer" }}
+          />
+        </ImageZoom>
+      </Modal>
     </div>
   );
 };
